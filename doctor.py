c12bb198309909c615f3501d3313788fe0887c52
Uwes Ahabab <uwesahabab@gmail.com>
2024-06-11 17:19:08 +0300

    Correct the file path for doctor.py file

--- doctor.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from . import Base
-
-class Doctor(Base):
-    __tablename__ = 'doctors'
-    id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
-    specialization = Column(String, nullable=False)
-    phone = Column(String, nullable=False)
