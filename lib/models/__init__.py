7db73693f7ee63614199f4935c1ee29312382359
John-Alexander Kamau <xander.kamau7@gmail.com>
2024-06-11 17:51:41 +0300

    Initializes database engine and session setup

---
+++ lib/models/__init__.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('sqlite:///hospital_inventory.db')
+Session = sessionmaker(bind=engine)
+Base = declarative_base()
+
+def init_db():
+    from .patient import Patient
+    from .doctor import Doctor
+    from .appointment import Appointment
+    from .user import User
+    from .role import Role
+    Base.metadata.create_all(engine)
+
