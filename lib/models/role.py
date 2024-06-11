df82e10eb1aedc02664c087fed9acb78c89e0c8d
John-Alexander Kamau <xander.kamau7@gmail.com>
2024-06-11 17:54:00 +0300

    Add Role model to manage user permissions and roles

---
+++ lib/models/role.py
@@ -0,0 +1,7 @@
+from sqlalchemy import Column, Integer, String
+from . import Base
+
+class Role(Base):
+    __tablename__ = 'roles'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, unique=True, nullable=False)
